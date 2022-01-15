c1c0fb5636d2ad0d7dfd1e2e0d5cb2083a956e32
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-01-15 22:58:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def reply_whatsapp():
           df = pd.DataFrame(output.json()['_items'])
           msg = response.message('------PLANT DISEASES MENU---------- \n ---------------------------------------------- \n' + str(df[['Code','Disease']]) + '\n ' + '------------------------------------------------ \n DSM -- to diseases Menu')
           
-        elif inc in dff.Code.values:
+        elif np.any(dff.Code.values in inc):
           pld_D = dff.loc[dff['Code'].str.contains(str(inc)),'Description']
           msg = response.message(str(pld_D))
         
