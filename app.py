7fcc07a3a160f7cfbcdd86473ef10bd441115753
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-01-19 12:50:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def reply_whatsapp():
             requests.post(url = 'https://lkdzzx.deta.dev/language_change/',params = {'user_number':phone_number,'lan' : 'english_vn'})
             
           msg = response.message('VN MENU')
-          msg.media('https://1atqmr.deta.dev/stream/first.wav')
+          msg.media('https://1atqmr.deta.dev/stream/first.mp4')
         
         else:
           output = 'still in development'
