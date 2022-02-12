7f06f29b8666b5cbf05a0f4abc0f31dd4a3a4a0b
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-02-12 22:39:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,17 @@ def reply_whatsapp():
           output = requests.get(url = "https://1atqmr.deta.dev/all_shops/")
           df = pd.DataFrame(output.json()['_items'])
           dist4 = pd.DataFrame({'District':df.District.value_counts().index.values})
-          msg = response.message("----------SHOPS MENU------------ \n --------------------------------------------\n" + str(dist4) + "\n " + "----------------------------------")
+          msg = response.message("----------DISTRICT MENU------------ \n --------------------------------------------\n" + str(dist4) + "\n " + "----------------------------------")
+          
+        elif (np.any(dfff11.District.values == inc) == True) & (dff3.loc[dff3['user_number'] == phone_number ,'lan'].values[0] == 'english'):
+          #output = requests.get(url = "https://1atqmr.deta.dev/all_shops/")
+          #df = pd.DataFrame(output.json()['_items'])
+          shop = dfff11.loc[dfff11['District'] == inc,'Shop'].values
+          df = pd.DataFrame({'Shops':shop})
+          msg = response.message("-----------------SHOPS MENU----------------- \n " + str(df) + "\n ----------------------------------------")
+          
+        #animals = dfff11.loc[dfff11['District'] == inc,'Description'].values
+        #msg = response.message(str(animals[0])) 
           
         elif ('gopr' in inc) & (dff3.loc[dff3['user_number'] == phone_number ,'lan'].values[0] == 'english'):
           msg = response.message('still under construction')
