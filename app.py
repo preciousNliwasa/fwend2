b38bbfef93e270c48c377d7642e6a34e62c1c06f
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-02-13 19:40:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def reply_whatsapp():
           output = requests.get(url = "https://1atqmr.deta.dev/get_products/")
           df = pd.DataFrame(output.json()['_items'])
           products = df.loc[df['Shop'] == inc]
-          msg = response.message("-------------------{0}---------------------- \n ".format(inc) + str(shop_descr.values[0]) + "\n ----------------------------------------------- \n " + str(products[["Product","Price"]]) + " \n --------------------------------------- \n SHP -- to shops menu")
+          msg = response.message("-------------------{0}---------------------- \n ".format(inc) + str(shop_descr.values[0]) + "\n ----------------------------------------------- \n " + str(products[["Product","Price"]]) + " \n ---------------------------------------")
           
           
         
