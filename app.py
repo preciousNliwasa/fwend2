fedf10d158b54afa5ee8a8c5e19631cc7bc838d5
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-03-08 19:50:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def reply_whatsapp():
         # using private network (prototype) 
         
         elif (('gopr' in inc) | ('mpn' in inc)) & (dff3.loc[dff3['user_number'] == phone_number ,'lan'].values[0] == 'english'):
-          msg = response.message("-----------MAIN MENU----------  \n ------------------------------------------------ \n Welcome to FarmConnect private network.Content is farmConnect users generated  \n  ------------------------------------------------- \n SCOH -- enter schools \n MARKT -- enter market \n GOPS -- enter groups \n ------------------------------------ \n CHL -- change channel")
+          msg = response.message("-----------MAIN MENU------------  \n ------------------------------------------------ \n Welcome to FarmConnect private network.Content is farmConnect users generated  \n  ------------------------------------------------- \n SCOH -- enter schools \n MARKT -- enter market \n GOPS -- enter groups \n ------------------------------------ \n CHL -- change channel")
          
         #elif ('scoh' in inc) & (dff3.loc[dff3['user_number'] == phone_number ,'lan'].values[0] == 'english'):
         #  output = requests.get(url = "https://qne10u.deta.dev/get_schools/")
